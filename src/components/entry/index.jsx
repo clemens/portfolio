c467c7fbeaf0a124e10ac19f1710840cae0555a0
Clemens Kofler <mail@clemenskofler.com>
2020-04-19 21:08:30 +0200

    Remove description

--- src/components/entry/index.jsx
+++ src/components/entry/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import classes from "./entry.module.css";
 
-const Entry = ({ desc = null, role = null, timeSpan = null, link = false, title, children }) => {
+const Entry = ({ role = null, timeSpan = null, link = false, title, children }) => {
   return (
     <div className={classes.wrapper}>
       {role ? (<h3 className={classes.entryRole}>{role}</h3>) : ''}
@@ -16,7 +16,6 @@ const Entry = ({ desc = null, role = null, timeSpan = null, link = false, title,
 
       {timeSpan ? (<p className={classes.timeSpan}>{timeSpan}</p>) : ''}
 
-      {desc ? (<p className={classes.desc}>{desc}</p>) : ''}
       {children ? (<div>{children}</div>)  :''}
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -50,89 +50,123 @@ const IndexPage = () => (
         title="CodeShip"
         link="https://codeship.com/"
         timeSpan="May 2014 – June 2016"
-        desc="Continuous Integration & Delivery platform; I supported CodeShip from May 2014 until June 2016 mainly with backend development."
-      />
+      >
+        Continuous Integration & Delivery platform; I supported CodeShip from May 2014 until June
+        2016 mainly with backend development.
+      </Entry>
       <Entry
         title="Mobilityhouse"
         link="https://www.mobilityhouse.com/"
         timeSpan="2014 – 2016"
-        desc="Provider of e-mobility solutions; I supported TMH as a lead architect and developer on 2 large-scale projects from 2014 until 2016."
-      />
+      >
+        Provider of e-mobility solutions; I supported TMH as a lead architect and developer on 2
+        large-scale projects from 2014 until 2016.
+      </Entry>
       <Entry
         title="bergshop.com"
         link="https://bergshop.com/"
         timeSpan="2012 – 2014"
-        desc="I built this online shop based on the Spree Commerce platform for a friend."
-      />
+      >I built this online shop based on the Spree Commerce platform for a friend.</Entry>
       <Entry
         title="Pernod Ricard Austria"
         link="https://www.pernod-ricard.at/"
         timeSpan="2013 – 2014"
-        desc="World's 2nd largest spirits producer; I built (concept & development) their service barkarte.at (defunct) in 2014/15, which was a web tool to create and order heavily customizable restaurant & bar menus (including an Adobe InDesign export API)."
-      />
+      >
+        Pernod Ricard is the world's 2nd largest spirits producer; I created (concept & development)
+        their service barkarte.at (defunct) in 2014/15, which was a web tool to create and order
+        heavily customizable restaurant & bar menus (including an Adobe InDesign export API). I was
+        also responsible for overall project management and coordination with stakeholders (creative
+        agency, printers etc.).
+      </Entry>
       <Entry
         title="Shore"
         link="https://www.shore.com/"
         timeSpan="November 2013 – March 2014"
-        desc="Business management suite; I supported Shore in their early days (when they were still called Termine24) from November 2013 until March 2014 with backend and frontend development."
-      />
+      >
+        Shore is a business management suite. I supported them in their early days (when they were
+        still called Termine24) from November 2013 until March 2014 with backend and frontend
+        development.
+      </Entry>
       <Entry
         title="fingerbook"
         link="https://www.fingerbook.com/"
         timeSpan="March 2012 – December 2013"
-        desc="Photobooklet creator; I developed all the backend and large parts of the frontend for fingerbook and helped with concept creation and UX from mid 2012 until end of 2013."
-      />
+      >
+        fingerbook is an online photobooklet creator; I developed all the backend and significant
+        parts of the frontend for fingerbook and helped with concept creation and UX from early 2012
+        until end of 2013.
+      </Entry>
       <Entry
-        title="meandyou"
+        title="MeAndYou"
         timeSpan="Winter 2011/12"
-        desc="Online dating platform (defunct); I did backend development, including a Redis-based profile matching algorithm, in winter 2011/12."
-      />
+      >
+        MeAndYou was an online dating platform. I did backend development for them, including a
+        Redis-based profile matching algorithm, in winter 2011/12.
+      </Entry>
       <Entry
         title="Tito"
         link="https://ti.to/"
         timeSpan="Summer 2011"
-        desc="Event management & ticket sales platform; I contributed to early development of Tito in Summer of 2011."
-      />
+      >
+        Tito is an event management & ticket sales platform; I contributed to early development of
+        Tito in Summer of 2011.
+      </Entry>
       <Entry
         title="Noko (Freckle) / Charm"
         link="https://nokotime.com/"
         timeSpan="2010/11"
-        desc="Time tracking app / customer support platform; I contributed development to Slash7's time tracking app Noko (then Freckle) and built the backend for the initial prototype of their discontinued Customer Support platform Charm in 2010 and 2011."
-      />
+      >
+        Time tracking app / customer support platform; I contributed development to Slash7's time
+        tracking app Noko (then Freckle) and built the backend for the initial prototype of their
+        discontinued Customer Support platform Charm in 2010 and 2011.
+      </Entry>
       <Entry
         title="Avarteq"
         link="https://avarteq.de/"
         timeSpan="2010 – 2014"
-        desc="I was hired as a consultant, coach and developer by Avarteq for various projects between 2010 and 2014."
-      />
+      >
+        I was hired as a consultant and developer as well as a coach and mentor for their junior
+        developers by Avarteq for various projects between 2010 and 2014.
+      </Entry>
       <Entry
         title="betterplace.org"
         link="https://www.betterplace.org/"
         timeSpan="January 2010"
-        desc="I helped betterplace.org in their challenges to upgrade from Rails 2.1 to 2.3 with a particular focus on getting i18n running properly."
-      />
+      >
+        I helped betterplace.org in their challenges to upgrade from Rails 2.1 to 2.3 with a
+        particular focus on getting i18n running properly.
+      </Entry>
       <Entry
         title="upstream"
         link="https://upstre.am"
         timeSpan="2010 – 2011"
-        desc="I helped my friends of upstream out with a couple of projects, including the reading platform PaperC and Twilio competitor Jamii."
-      />
+      >
+        I helped my friends of upstream out with a couple of projects, including the reading
+        platform PaperC and Twilio competitor Jamii.
+      </Entry>
       <Entry
         title="COMMUNITOR"
         link="https://communitor.com/"
         timeSpan="2007 – 2014"
-        desc="Between 2007 and 2014, I was hired repeatedly by COMMUNITOR as a consultant and developer on various projects, including projects for the Austrian Ministry of Education, beauty.at and auto-motor.at, and contributed concepts and development for their proprietary CMS SiteWare."
-      />
+      >
+        Between 2007 and 2014, I was hired repeatedly by COMMUNITOR as a consultant and developer on
+        various projects, including projects for the Austrian Ministry of Education, beauty.at and
+        auto-motor.at, and contributed concepts and development for their proprietary CMS SiteWare.
+      </Entry>
       <Entry
         title="BESTgroup / advaBEST"
         timeSpan="2008 – 2010"
-        desc="I was hired as a developer to work on the adva-cms content management platform (open source) and the advaBEST business platform (proprietary)."
-      />
+      >
+        I was hired as a developer to work on the adva-cms content management platform (open source)
+        and the advaBEST business platform (proprietary).
+      </Entry>
       <Entry
         title="Power4Youth.eu"
         timeSpan="2006 – 2007"
-        desc="Power4Youth was a project sponsored by SOS Children's Villages to create a community for children brought up in institutions. I built the whole platform start to finish."
-      />
+      >
+        Power4Youth was a project sponsored by SOS Children's Villages to create a community for
+        children brought up in institutions. I built the whole platform start to finish.
+      </Entry>
     </Section>
     <Section id="open-source" title="Open Source & Blogging">
       <Entry
@@ -143,38 +177,48 @@ const IndexPage = () => (
       <Entry
         title="Steam"
         link="https://github.com/svenfuchs/steam"
-        desc="In 2009, Steam was one of the first tools in the Ruby ecosystem to allow for (headless) JavaScript-enabled testing. I co-authored the tool together with Sven Fuchs."
-      />
+      >
+        In 2009, Steam was one of the first tools in the Ruby ecosystem to allow for (headless)
+        JavaScript-enabled testing. I co-authored the tool together with Sven Fuchs.
+      </Entry>
       <Entry
         title="Ruby i18n"
         link="https://github.com/ruby-i18n/i18n"
-        desc="I was a core contributor to the Ruby i18n package. Among other things, I contributed the cascading backend."
-      />
+      >
+        I was a core contributor to the Ruby i18n package. Among other things, I contributed the
+        cascading backend.
+      </Entry>
       <Entry
         title="delocalize"
         link="https://github.com/clemens/delocalize"
-        desc="Library for parsing localized dates/times and numbers into internationalized formats."
-      />
-      <Entry
-        title="Others"
-        link="https://github.com/clemens?language=&q=&tab=repositories&type=source"
-        desc="There's a handful of other things I've written and experimented with over the years. Check out my GitHub profile for a more exhaustive list."
-      />
+      >
+        Library for parsing localized dates/times and numbers into internationalized formats.
+      </Entry>
+      <Entry title="Others">
+        There's a handful of other things I've written and experimented with over the years. Check
+        out <a href="https://github.com/clemens?language=&q=&tab=repositories&type=source" 
+        target="_blank">my GitHub profile</a> for a more exhaustive list.
+      </Entry>
     </Section>
     <Section id="education" title="Education">
       <Entry
         title="Diploma / Mag. (FH) in Management & Applied Informatics"
         link="https://mci.edu/"
         timeSpan="2004 – 2008"
-        desc="I studied Management & Applied Informatics at Management Center Innsbruck (with a semester abroad in Brisbane, Australia) and graduated with a diploma in 2008. My thesis analyzed usability concerns for websites with heterogeneous target groups."
-      />
+      >
+        I studied Management & Applied Informatics at Management Center Innsbruck (with a semester
+        abroad in Brisbane, Australia) and graduated with a diploma in 2008. My thesis analyzed
+        usability concerns for websites with heterogeneous target groups.
+      </Entry>
     </Section>
     <Section id="skills" title="Skills">
       <Entry
         title="Languages & Frameworks"
-        desc="Ruby on Rails, Vue.js, JavaScript (ES6+) and some basics in Laravel and React"
-      />
-      <Entry title="Data Stores" desc="PostreSQL, Redis, MySQL, MongoDB" />
+      >
+        Ruby on Rails, Vue.js, JavaScript (ES6+) and some basics in Laravel and React.
+        (And yes, of course I can do jQuery, too.)
+      </Entry>
+      <Entry title="Data Stores">PostreSQL, Redis, MySQL, MongoDB</Entry>
     </Section>
     <Section id="imprint" title="Imprint / Impressum">
       <p>
