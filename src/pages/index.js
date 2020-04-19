1348d2e0c9f5125041c05bffaa64d2f0d207cb5e
Clemens Kofler <mail@clemenskofler.com>
2020-04-19 17:11:24 +0200

    Fill with relevant content

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,69 +12,90 @@ const IndexPage = () => (
     <SEO title="Clemens Kofler" />
     <Section id="about" title="About Me">
       <p>
-        I'm a motivated full-stack engineer who has experience building and
-        scaling web applications and services in various domains.
+        I'm a seasoned technologist and entrepreneur with almost 15 years experience of building
+        solutions for various startups (including my own) as well as small and mid-sized companies.
       </p>
       <p>
-        I'm also passionate about open source contributions, working in high
-        impact environments with frequent deployments, and helping growing
-        development teams build great products.
+        I'm also passionate about open source contributions and I've authored and contributed to
+        multiple open source projects, including Ruby on Rails and Ruby internationalization (i18n).
       </p>
     </Section>
     <Section id="experience" title="Experience">
       <Link
-        title="Ambyint"
-        link="https://ambyint.com"
-        desc="Full-Stack Developer, September 2018 - Present"
+        title="Temedica"
+        link="https://temedica.com"
+        desc="Co-Founder, CTO and Managing Director; 2016 - Present"
       />
       <Link
-        title="Bank of Canada"
-        link="https://bankofcanada.ca"
-        desc="Full-Stack Developer, January 2018 - September 2018"
-      />
-      <Link
-        title="Bank of Canada"
-        link="https://bankofcanada.ca"
-        desc="Full-Stack Developer (Co-op), May 2015 - August 2017"
+        title="Freelancer"
+        link="https://www.railway.at"
+        desc="Consulting & Development; 2005 - 2016"
       />
     </Section>
-    <Section id="projects" title="Projects">
+    <Section id="freelancing-projects" title="Freelancing Projects/Clients">
       <Link
-        title="Pivalink"
-        link="https://pivalink.com"
-        desc="Tool to create microlanding pages that can be easily updated and customized to fit any brand"
+        title="CodeShip"
+        link="https://codeship.com/"
+        desc="Continuous Integration & Delivery platform; I supported CodeShip from May 2014 until June 2016 mainly with backend development."
       />
       <Link
-        title="Coursefacts"
-        link="https://coursefacts.com"
-        desc="A platform where students can read and write university / college course reviews anonymously"
+        title="Mobilityhouse"
+        link="https://www.mobilityhouse.com/"
+        desc="Provider of e-mobility solutions; I supported TMH as a lead architect and developer on 2 large-scale projects from 2014 until 2016."
       />
       <Link
-        title="Dev Blog"
-        link="https://github.com/RyanFitzgerald/devblog"
-        desc="A open source and minimal personal blog template for developers built with Gatsby and React"
+        title="Pernod Ricard Austria"
+        link="https://www.pernod-ricard.at/"
+        desc="World's 2nd largest spirits producer; I built (concept & development) their service barkarte.at (defunct) in 2014/15, which was a web tool to create and order heavily customizable restaurant & bar menus (including an Adobe InDesign export API)."
       />
       <Link
-        title="Dev Portfolio"
-        link="https://github.com/RyanFitzgerald/devportfolio"
-        desc="An open source and customizable single-page personal portfolio template for developers"
+        title="Shore"
+        link="https://www.shore.com/"
+        desc="Business management suite; I supported Shore in their early days (when they were still called Termine24) from November 2013 until March 2014 with backend and frontend development."
       />
       <Link
-        title="Day Counter"
-        link="https://chrome.google.com/webstore/detail/day-counter-new-tab-page/popaiegponeiefbiddhmaphpbdjoegff?hl=en"
-        desc="Chrome extension to track days until or since life events with over 5k active users"
+        title="fingerbook"
+        link="https://www.fingerbook.com/"
+        desc="Photobooklet creator; I developed all the backend and large parts of the frontend for fingerbook and helped with concept creation and UX from mid 2012 until end of 2013."
+      />
+      <Link
+        title="meandyou"
+        desc="Online dating platform (defunct); I did backend development, including a Redis-based profile matching algorithm, in winter 2011/12."
+      />
+      <Link
+        title="Tito"
+        link="https://ti.to/"
+        desc="Event management & ticket sales platform; I contributed to early development of Tito in Summer of 2011."
+      />
+      <Link
+        title="Noko (Freckle) / Charm"
+        link="https://nokotime.com/"
+        desc="Time tracking app / customer support platform; I contributed development to Slash7's time tracking app Noko (then Freckle) and built the backend for the initial prototype of their discontinued Customer Support platform Charm in 2010 and 2011."
+      />
+      <Link
+        title="Avarteq"
+        link="https://avarteq.de/"
+        desc="I was hired as a consultant, coach and developer by Avarteq for various projects between 2010 and 2014."
+      />
+      <Link
+        title="COMMUNITOR"
+        link="https://communitor.com/"
+        desc="Between 2007 and 2014, I was hired repeatedly by COMMUNITOR as a consultant and developer on various projects, including projects for the Austrian Ministry of Education, beauty.at and auto-motor.at, and contributed concepts and development for their proprietary CMS SiteWare."
+      />
+    </Section>
+    <Section id="education" title="Education">
+      <Link
+        title="Diploma / Mag. (FH) in Management & Applied Informatics"
+        link="https://mci.edu/"
+        desc="I studied Management & Applied Informatics at Management Center Innsbruck (with a semester abroad in Brisbane, Australia) and graduated with a diploma in 2008. My thesis analyzed usability concerns for websites with heterogeneous target groups."
       />
     </Section>
     <Section id="skills" title="Skills">
       <Link
         title="Languages & Frameworks"
-        desc="JavaScript (ES6+), Python, React, Node.js, Express.js, Ruby on Rails, PHP"
+        desc="Ruby on Rails, Vue.js, JavaScript (ES6+) and some basics in Laravel and React"
       />
-      <Link title="Databases" desc="MongoDB, PostreSQL, MySQL" />
-      <Link
-        title="Other"
-        desc="Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum"
-      />
+      <Link title="Data Stores" desc="PostreSQL, Redis, MySQL, MongoDB" />
     </Section>
   </Layout>
 );
