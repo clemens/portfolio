31ff436e15d56ef51b6cef1d204029835738c4fa
Clemens Kofler <mail@clemenskofler.com>
2021-01-25 15:42:47 +0100

    Create uc_test.js

---
+++ src/pages/uc_test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+const UcTestPage = () => (
+  <Layout>
+    <script id="usercentrics-cmp" data-settings-id="pU78ZrwFh" src="https://app.usercentrics.eu/browser-ui/latest/bundle.js" defer></script>
+    <p>Just testing!</p>
+  </Layout>
+);
+
+export default UcTestPage;
